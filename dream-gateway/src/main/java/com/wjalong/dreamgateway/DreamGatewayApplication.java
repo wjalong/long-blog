a4c641f5aa24bb707180ef5cd70e7e996f36af00
wjalong <376183066@qq.com>
2022-07-13 19:02:06 -0400

    20220713:重构项目

---
+++ dream-gateway/src/main/java/com/wjalong/dreamgateway/DreamGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.wjalong.dreamgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamGatewayApplication.class, args);
+	}
+
+}
---
+++ dream-gateway/src/test/java/com/wjalong/dreamgateway/DreamGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjalong.dreamgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
